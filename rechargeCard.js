3f880b04500a3dd0add0ac4de7415f701aad1cb0
anigilaje ridwan <anigilajeridwanhorpeyemy@gmail.com>
2023-04-17 13:43:03 +0100

    i've added recharge pin function

--- rechargeCard.js
+++ rechargeCard.js
@@ -1,8 +1,24 @@
 let display = document.getElementById('display')
 let amountRecharge = document.getElementById('amountRecharge')
+// let getRandom = document.getElementById('getRandom')
+let generatePinInput = document.getElementById("generatePinInput");
+let chooseNetwork = document.getElementById('chooseNetwork')
+let rechargeInput = document.getElementById('rechargeInput')
+
+let data = []
 
 
-function random() {
+function getRandom() {
     return Math.floor(Math.random() * 10000000000)
-    
+}
+
+function generatePin(){
+     generatePinInput.value = getRandom();
+}
+function rechargeCard() {
+    rechargeInput.value = ''
+    if (rechargeInput.value = ` *555* ${generatePinInput.value}#`) {
+        alert('yes valid')
+        
+    }
 }
